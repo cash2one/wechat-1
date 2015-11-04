52c770c5403376df737605a755391ca4463a3863
zhaoxuan <zhaoxuan1727@gmail.com>
2015-11-04 22:06:15 +0800

    add page

--- tmp/phantomjs.py
+++ tmp/phantomjs.py
@@ -2,9 +2,9 @@ from tornado_fetcher import Fetcher
 
 fetcher=Fetcher(
     user_agent='phantomjs',
-    phantomjs_proxy='http://localhost:12306',
+    phantomjs_proxy='http://192.168.7.111:9999',
     poolsize=10,
     async=False
     )
 
-print fetcher.phantomjs_fetch(url)
+print fetcher.phantomjs_fetch('www.baidu.com')
