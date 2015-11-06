001ad9cef7bb8cf18f5ca178088ae9f61179fc1f
zhaoxuan <zhaoxuan1727@gmail.com>
2015-11-06 20:46:31 +0800

    delete cache class object

--- lib/task_cache.py
+++ lib/task_cache.py
@@ -4,7 +4,6 @@ import redis
 
 class TaskCache(object):
     """docstring for TaskCache"""
-    cache = redis.Redis(db = 0)
 
     def __init__(self, host = '127.0.0.1', port = 6309, db = 0):
         self.cache = redis.Redis(host = host, port = port, db = db)
@@ -31,36 +30,6 @@ class TaskCache(object):
 
     def is_empty(self):
         key_size = self.cache.dbsize()
-        if key_size == 0:
-            return True
-        else:
-            return False
-
-    @classmethod
-    def get(cls):
-        key_size = cls.cache.dbsize()
-        if key_size > 0:
-            key = cls.cache.randomkey()
-            value = cls.cache.get(key)
-            cls.cache.delete(key)
-            return value
-        else:
-            return None
-
-    @classmethod
-    def push(cls, url):
-        m1 = md5.new()
-        m1.update(url)
-        md5_value = m1.hexdigest()
-        if cls.cache.exists(md5_value):
-            pass
-        else:
-            cls.cache.set(md5_value, url)
-            pass
-
-    @classmethod
-    def is_empty(cls):
-        key_size = cls.cache.dbsize()
         if key_size == 0:
             return True
         else:
