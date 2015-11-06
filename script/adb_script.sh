131be1ccf41a9bd737fdecf05f1d23c591cefdde
john <zhaoxuan1727@gmail.com>
2015-11-06 14:24:30 +0800

    add android shell for simulating tap

---
+++ script/adb_script.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#
+# adb_script.sh
+# Copyright (C) 2015 john <john@forever>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+while true;do
+	adb shell input touchscreen tap 357 853
+	sleep 2
+	adb shell input touchscreen tap 49 96
+	sleep 1
+done
