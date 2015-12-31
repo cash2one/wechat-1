aab1cdf8c6b31632eca3638d9b498d56d38e0709
zhaoxuan <zhaoxuan1727@gmail.com>
2016-01-01 00:02:35 +0800

    write to file

--- selenium_spider.py
+++ selenium_spider.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(800, 600))
+display = Display(visible=0, size=(720, 1280))
 display.start()
 driver = webdriver.Chrome()
 
-driver.get("http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzA4ODQ3MjY4MQ==&uin=MTIzOTkyMDUzOQ%3D%3D&key=41ecb04b05111003345d8fc7d7d8c39c83315cf8403b7acc4ce5c0f102e0e77e4e51d36f1d958be8073697d45c63ffcd&devicetype=android-19&version=26030849&lang=zh_CN&nettype=WIFI&pass_ticket=UTTrhEGgJukIpA6VoibWbq1t7FrjhpKFkGyNmVCFd0yjBD6ic2oKw1%2FAoRwvSlbT")
+driver.get("http://www.baidu.com")
 
-print driver.page_source
+f = open("baidu.html", "w")
+f.write(driver.page_source)
+f.close()
 driver.close()
