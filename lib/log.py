b4e2c88c50fa9c26d9eb6f2a238e02608b1c269c
zhaoxuan <zhaoxuan1727@gmail.com>
2016-01-30 00:52:49 +0800

    get log config from config.py

--- lib/log.py
+++ lib/log.py
@@ -1,12 +1,5 @@
 #coding:utf-8
 import logging
-import logging.handlers
-
-LOG_FILE = 'log/api.log'
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes = 1024*1024, backupCount = 5)
-fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-formatter = logging.Formatter(fmt)
-handler.setFormatter(formatter)
 
 class Log(object):
     """docstring for Log"""
