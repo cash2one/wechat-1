1472e5d308ce1ee092f79b9328b4e6d78bedbd63
john <zhaoxuan1727@gmail.com>
2016-01-30 01:06:43 +0800

    use config.py

--- lib/log.py
+++ lib/log.py
@@ -1,12 +1,15 @@
 #coding:utf-8
 import logging
+import sys
+sys.path.append("..")
+import config
 
 class Log(object):
     """docstring for Log"""
     def __init__(self, class_name):
         self.class_name = class_name
         self.logger = logging.getLogger(self.class_name)
-        self.logger.addHandler(handler)
+        self.logger.addHandler(config.handler)
 
     def info(self, message):
         self.logger.info(message)
